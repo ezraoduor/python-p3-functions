0b103a24dbf7ad94e41681159daa151af81a9c87
Ezra Oduor <ezrachurchill20@gmail.com>
2025-05-09 15:07:04 +0300

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello,", end=" ")
+    print("programmer", end="!\n")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!", end="\n")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!", end="\n")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
